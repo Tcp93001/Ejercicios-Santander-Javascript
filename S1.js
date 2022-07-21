9f5107fa52bce33d9e58c52cecdb01d8f78e9c96
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2022-07-20 19:28:46 -0500

    segunda sesion ejercicios y retos

---
+++ S1.js
@@ -0,0 +1,18 @@
+// Precedencia de operadores
+let a = 5, b = 10, c = 15;
+
+// let a = 5:
+// let b = 10:
+// let c = 15;
+
+(3 + b) * c / a * 2; // 78
+
+((a + b * c) / 5) * 2; // 62
+
+b / a + 2 * c; // 32
+
+(a + b + c / c) * a; // 80
+
+((3 * a) / c) + a + b + c; // 31
+
+a - (b + c) * a / 1; // -120
---
+++ S2results.js
@@ -0,0 +1,95 @@
+// Ejemplo 1
+// const time = 13;
+// let greeting;
+
+// if (time < 12) {
+//   greeting = "Good morning";
+// }
+
+// if (time < 20) {
+//   greeting = "Good afternoon";
+// }
+
+// if (time >= 20) {
+//   greeting = "Good evening";
+// }
+
+// console.log(greeting) // Good afternoon
+
+// if (time < 12) {
+//   greeting = "Good morning";
+// } else if (time < 20) {
+//   greeting = "Good afternoon";
+// } else if (time >= 20) {
+//   greeting = "Good evening";
+// }
+
+
+//Reto 1
+// const time = 13;
+// let greeting;
+
+// if (time < 12) {
+//   greeting = "Good morning";
+// } else if (time < 20) {
+//   greeting = "Good afternoon";
+// } else if (time >= 20) {
+//   greeting = "Good evening";
+// }
+
+// console.log(greeting) // Good afternoon
+
+// EJEMPLO 2
+
+// const day = 2;
+// let text;
+
+// switch (day) {
+//   case 0:
+//     text = "Sunday";
+//     break;
+//   case 1:
+//     text = "Monday";
+//     break;
+//   case 2:
+//     text = "Tuesday";
+//     break;
+//   case 3:
+//     text = "Wednesday";
+//     break;
+//   case 4:
+//     text = "Thursday";
+//     break;
+//   case 5:
+//     text = "Friday";
+//     break;
+//   case 6:
+//     text = "Saturday";
+//     break;
+//   default:
+//     text = "Error";
+
+// }
+
+// console.log(text); // Tuesday
+
+// RETO 2
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) console.log('numero par', i)
+// }
+
+// RETO 3
+
+// for(let counter = 2; counter <= 100; counter++){
+//     let isPrime = true;
+
+//     for(let i = 2; i <= counter; i++) {
+//         if(counter % i === 0 && i !== counter){
+//             isPrime = false;
+//         }
+//     }
+
+//     if (isPrime) console.log('Numero primo', counter)
+
+// }
